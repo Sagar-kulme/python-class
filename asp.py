f33c9cd8a5d391fc33396f8b43b1f3c15856a71d
Sagar-kulme <sagarkulme2222@gmail.com>
2025-08-04 19:45:34 +0530

    win & drow game

---
+++ asp.py
@@ -0,0 +1,8 @@
+# win and drow game 
+import random
+guss=int(input("Enter your gussing "))
+number=random.randint(1,10)
+if(guss==number):
+    print("Congrulation you win 20 % Discount")
+else:
+    print("Butter luck next time ")
